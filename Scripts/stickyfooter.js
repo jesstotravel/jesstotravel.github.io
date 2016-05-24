0598ee974416dce7a1d6029c26de69ce421828dc
JT <jesstotravel@gmail.com>
2016-05-24 13:41:07 +1000

    upload site

---
+++ Scripts/stickyfooter.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+        
+              // sticky footer scroll effect
+              $(document).scroll(function() {
+                if($('.nav__wrapper').offset().top + $('.nav__wrapper').height() >= $('footer').offset().top - 10) {
+                  $('.nav__wrapper').css('position', 'absolute');
+                }
+                  
+                if($(document).scrollTop() + window.innerHeight < $('footer').offset().top) {
+                  $('.nav__wrapper').css('position', 'fixed'); // restore when you scroll up
+                }
+              });
+        });
